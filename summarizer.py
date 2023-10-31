c459915427c26ae2bf3cbbd8be9359ca3a73cafa
Kranthi Kiran <getjkranthi@gmail.com>
2023-10-31 19:06:32 -0300

    Removed local API_key retrieval

--- summarizer.py
+++ summarizer.py
@@ -27,7 +27,7 @@ prompt = PromptTemplate(
     template = template,
 )
 
-OPENAI_api_key = os.getenv('OPENAI_API_KEY')
+#OPENAI_api_key = os.getenv('OPENAI_API_KEY')
 if (st.secrets["temp_key"]):
     st.write(os.getenv('OPENAI_API_KEY') == st.secrets["temp_key"])
     OPENAI_api_key = os.getenv('OPENAI_API_KEY')
