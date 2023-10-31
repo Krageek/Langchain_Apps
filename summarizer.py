e2efe6f022fd6244ba383bbc0291da1a461f6e02
Kranthi Kiran <getjkranthi@gmail.com>
2023-10-31 19:03:25 -0300

    Modified api_key retrieval for Streamlit

--- summarizer.py
+++ summarizer.py
@@ -27,8 +27,12 @@ prompt = PromptTemplate(
     template = template,
 )
 
-#OPENAI_api_key = os.getenv('OPENAI_API_KEY')
-OPENAI_api_key = os.environ.get("OPENAI_API_KEY")
+OPENAI_api_key = os.getenv('OPENAI_API_KEY')
+if (st.secrets["temp_key"]):
+    st.write(os.getenv('OPENAI_API_KEY') == st.secrets["temp_key"])
+    OPENAI_api_key = os.getenv('OPENAI_API_KEY')
+else:
+    st.write("API_key not found")
 
 
 def load_openAI():
