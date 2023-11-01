a044e58ff685b69567e449529043ba264dd2b59d
Kranthi Kiran <getjkranthi@gmail.com>
2023-11-01 03:28:57 -0300

    Formatted prompt output

--- summarizer.py
+++ summarizer.py
@@ -17,9 +17,12 @@ template = """
 
     Text: {text}
 
-    Remember that your summary has to be in bullet points with keywords after that. Each bullet point of your summary has to be in a new line. It should be formatted like the example below:
-    • This is the first sentence of the summary
-    • This is the second sentence of the summary
+    Remember that your summary has to be in bullet points with keywords after that. It is absolutely essential that every bullet point of your summaryappears in a new line. 
+    
+    It should be formatted like the example below:
+    • This is the first sentence of the summary and is part of the first line only.
+
+    • This is the second sentence of the summary and is part of the second line only.
     
     Keywords:
 
